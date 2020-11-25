353683436c36ec7dfb54208099267b01bcb8f4e5
TokyoDiary <106933319+TokyoDiary@users.noreply.github.com>
2020-11-25 16:57:12 +0000

    fixing build errors

--- _example/sample.go
+++ _example/sample.go
@@ -17,3 +17,27 @@ type Momento struct {
 }
 
 // OnTick Method
+func (a *Momento) OnTick(t kstreamdb.TickData, b *malgova.Book) {
+	if t.TradingSymbol == a.symbol {
+		a.cs1m.Update(t)
+	}
+}
+
+// OnClose method
+func (a *Momento) OnClose(b *malgova.Book) {
+	b.Exit()
+}
+
+// OnPeriodic method
+func (a *Momento) OnPeriodic(t time.Time, b *malgova.Book) {
+	if a.cs1m.HasChanged(t) && len(a.cs1m.Close) > 15 {
+		ltp := a.cs1m.LTP
+		ma1 := talib.Sma(a.cs1m.High, 15)
+		ma2 := talib.Ema(a.cs1m.Close, 15)
+		ma3 := talib.Sma(a.cs1m.Low, 15)
+		if b.IsBookClean() && talib.Crossover(ma2, ma1) {
+			//fmt.Printf("[%v] Buy @ %.2f\n", t, ltp)
+			quantityToBuy := int(b.Cash / ltp)
+			b.Buy(quantityToBuy)
+		}
+		if b.InPosition() && talib.Crossunder(ma2, ma3) {

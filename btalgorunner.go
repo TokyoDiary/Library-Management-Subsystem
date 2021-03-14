356b2d8af7052f8634926880a7f6a819be9d33f4
TokyoDiary <106933319+TokyoDiary@users.noreply.github.com>
2021-03-14 22:56:01 +0000

    Update elektrosmoker.xml

--- btalgorunner.go
+++ btalgorunner.go
@@ -45,4 +45,4 @@ func (a *btAlgoRunner) run() {
 		}
 		a.strategy.OnDayEnd(&a.book)
 		a.resetQueue()
-		//fmt.Printf("P/L %9.2f | Trades %3d | 
+		//fmt.Printf("P/L %9.2f | Trades %3d | %s\n", a.book

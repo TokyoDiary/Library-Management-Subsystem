31203447f0f28d1aaf8da95ef422ac3d50b36534
TokyoDiary <106933319+TokyoDiary@users.noreply.github.com>
2021-04-05 10:59:55 +0000

    ipv4 igmp: use in_dev_put in timer handlers instead of __in_dev_put

--- btalgorunner.go
+++ btalgorunner.go
@@ -58,4 +58,5 @@ func (a *btAlgoRunner) exit() {
 
 func (a *btAlgoRunner) checkClock(t time.Time) {
 	utcNow := t.Unix()
-	if a.utcLastPeriodicCall 
+	if a.utcLastPeriodicCall < utcNow {
+		a.utcLastPeri

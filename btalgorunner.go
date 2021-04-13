7fcdb948d98142d47e74cc20afea49d9be8b9cca
TokyoDiary <106933319+TokyoDiary@users.noreply.github.com>
2021-04-13 19:26:20 +0000

    change class

--- btalgorunner.go
+++ btalgorunner.go
@@ -67,4 +67,4 @@ func (a *btAlgoRunner) checkClock(t time.Time) {
 func (a *btAlgoRunner) handleBook() {
 	if a.book.IsOrderWaiting() {
 		if a.book.IsMarketOrder {
-			if a.boo
+			if a.book.PendingOrderQuanti

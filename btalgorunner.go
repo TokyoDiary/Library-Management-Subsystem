985a7ce99d95ca68584956f47f676bc8ce052e1c
TokyoDiary <106933319+TokyoDiary@users.noreply.github.com>
2021-07-04 21:38:38 +0000

    Correct spelling.

--- btalgorunner.go
+++ btalgorunner.go
@@ -110,4 +110,5 @@ func (a *btAlgoRunner) handleBook() {
 		} else {
 			if a.book.PendingOrderQuantity > 0 {
 				if a.lastTick.LastPrice <= float32(a.book.PendingOrderPrice) {
-					cost := a.book.PendingOrderPrice * float64(a.book.Pending
+					cost := a.book.PendingOrderPrice * float64(a.book.PendingOrderQuantity)
+					a.book.

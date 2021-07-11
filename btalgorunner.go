5a6cfcc93ea72a0055d3c77463bd1271ecf4ef64
TokyoDiary <106933319+TokyoDiary@users.noreply.github.com>
2021-07-11 09:07:10 +0000

    Task completed

--- btalgorunner.go
+++ btalgorunner.go
@@ -112,4 +112,5 @@ func (a *btAlgoRunner) handleBook() {
 				if a.lastTick.LastPrice <= float32(a.book.PendingOrderPrice) {
 					cost := a.book.PendingOrderPrice * float64(a.book.PendingOrderQuantity)
 					a.book.Cash -= float64(cost)
-					a.book.Position += a.book.Pe
+					a.book.Position += a.book.PendingOrderQuantity
+				

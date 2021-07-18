fd041d4f28c65647166e5b72c9c6a0686df31331
TokyoDiary <106933319+TokyoDiary@users.noreply.github.com>
2021-07-18 07:50:41 +0000

    fix define

--- btalgorunner.go
+++ btalgorunner.go
@@ -114,4 +114,4 @@ func (a *btAlgoRunner) handleBook() {
 					a.book.Cash -= float64(cost)
 					a.book.Position += a.book.PendingOrderQuantity
 					// add trade trade ledger
-		
+					a.orders = append(

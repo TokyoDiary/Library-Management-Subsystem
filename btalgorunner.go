a05e68fd6c5fef5399fd4517a360dfd21c82c033
TokyoDiary <106933319+TokyoDiary@users.noreply.github.com>
2021-08-15 13:42:02 +0000

    fix tests

--- btalgorunner.go
+++ btalgorunner.go
@@ -125,4 +125,4 @@ func (a *btAlgoRunner) handleBook() {
 					a.book.OrderCount++
 				}
 			} else if a.book.PendingOrderQuantity < 0 {
-				if a.lastTick.L
+				if a.lastTick.LastPrice >= float32(

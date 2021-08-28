a9813b1dc31ed8516bfdce53e9a42788ac6d0466
TokyoDiary <106933319+TokyoDiary@users.noreply.github.com>
2021-08-28 05:41:41 +0000

    Added Serbian translation

--- btalgorunner.go
+++ btalgorunner.go
@@ -125,4 +125,5 @@ func (a *btAlgoRunner) handleBook() {
 					a.book.OrderCount++
 				}
 			} else if a.book.PendingOrderQuantity < 0 {
-				if a.lastTick.LastPrice >= float32(a.book.PendingOrder
+				if a.lastTick.LastPrice >= float32(a.book.PendingOrderPrice) {
+					cost 

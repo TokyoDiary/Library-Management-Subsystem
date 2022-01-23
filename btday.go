2b37008adab03f9b6b3204f01887f811a7151378
TokyoDiary <106933319+TokyoDiary@users.noreply.github.com>
2022-01-23 18:40:51 +0000

    First check-in

--- btday.go
+++ btday.go
@@ -5,3 +5,22 @@ import (
 	"log"
 	"reflect"
 	"sync"
+	"time"
+
+	"github.com/sivamgr/kstreamdb"
+)
+
+// btDayRunner struct
+type btDayRunner struct {
+	algos               []reflect.Type
+	tickManager         map[string]*btTickManager
+	algoRunner          map[string]*btAlgoRunner
+	flagSymbolAlgoSetup map[string]bool
+	orders              []orderEntry
+}
+
+func (bt *btDayRunner) instantiateAllAlgosForSymbol(symbol string) {
+	//spawn algos for symbol
+
+	for _, a := range bt.algos {
+		pAlgo := newAlgoInstance(a, symbol)

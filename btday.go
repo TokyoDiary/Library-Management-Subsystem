830f3bd452094ab067b26fd0dc1f48d7f77ea7b4
TokyoDiary <106933319+TokyoDiary@users.noreply.github.com>
2022-01-26 09:37:17 +0000

    Fixing errors

--- btday.go
+++ btday.go
@@ -24,3 +24,24 @@ func (bt *btDayRunner) instantiateAllAlgosForSymbol(symbol string) {
 
 	for _, a := range bt.algos {
 		pAlgo := newAlgoInstance(a, symbol)
+		algoID := pAlgo.ID()
+		bt.algoRunner[algoID] = pAlgo
+		for _, w := range pAlgo.watch {
+			if _, ok := bt.tickManager[w]; !ok {
+				bt.tickManager[symbol] = new(btTickManager)
+			}
+			bt.tickManager[symbol].addObserver(algoID)
+		}
+	}
+}
+
+// worker for concurrent algo execution
+func algoRunWorker(wg *sync.WaitGroup, algo *btAlgoRunner, bt *btDayRunner) {
+	defer wg.Done()
+	algo.run()
+}
+
+func (bt *btDayRunner) setup(algos []reflect.Type) {
+	bt.algos = algos
+	bt.tickManager = make(map[string]*btTickManager)
+	bt.algoRunner = make(map[string]*btAlgoRunner)

8b2d172faf9c49a174818970f126e020f9a2c4be
TokyoDiary <106933319+TokyoDiary@users.noreply.github.com>
2022-02-12 20:21:00 +0000

    Bump up version

--- btengine.go
+++ btengine.go
@@ -22,3 +22,24 @@ func (bt *BacktestEngine) RegisterAlgo(a interface{}) {
 	if bt.algos == nil {
 		bt.algos = make([]reflect.Type, 0)
 	}
+	bt.algos = append(bt.algos, reflect.TypeOf(a))
+}
+
+// RunAlgoBetweenDate method
+func (bt *BacktestEngine) RunAlgoBetweenDate(feed *kstreamdb.DB, oms OrderManager, algoName string, startDate time.Time, endDate time.Time) {
+	selectedAlgo := make([]reflect.Type, 0)
+	for _, a := range bt.algos {
+		if a.Name() == algoName {
+			selectedAlgo = append(selectedAlgo, a)
+			break
+		}
+	}
+
+	if len(selectedAlgo) == 0 {
+		return
+	}
+
+	dates, _ := feed.GetDates()
+	dayRunner := btDayRunner{}
+	dayRunner.setup(selectedAlgo)
+	log.SetFlags(log.LstdFlags | log.Lmicroseconds)

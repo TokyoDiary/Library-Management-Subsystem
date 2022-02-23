c7c61685616f3b8bdc038aeacd9f8cc9aabccd58
TokyoDiary <106933319+TokyoDiary@users.noreply.github.com>
2022-02-23 08:39:38 +0000

    111111

--- candle.go
+++ candle.go
@@ -16,3 +16,33 @@ type CandleStick struct {
 	C float64
 	V uint32
 }
+
+// CandlesData struct
+type CandlesData struct {
+	marketStartTimeHour   int
+	marketStartTimeMinute int
+
+	dayStartTime time.Time
+	candlePeriod int
+
+	currentCandle              CandleStick
+	currentCandleHarvestTime   time.Time
+	currentCandleTicksReceived uint32
+
+	totalVolumeTraded uint32
+	Candles           []CandleStick
+	Open              []float64
+	High              []float64
+	Low               []float64
+	Close             []float64
+	Volume            []float64
+	LTP               float64
+}
+
+// HasChanged method, call with timestamp, to harvest candless
+// return true is a new candle is formed
+func (f *CandlesData) HasChanged(t time.Time) bool {
+	if (!t.Before(f.currentCandleHarvestTime)) && f.currentCandleTicksReceived > 0 {
+		f.Candles = append(f.Candles, f.currentCandle)
+		f.Open = append(f.Open, f.currentCandle.O)
+		f.High = append(f.High, f.currentCandle.H)

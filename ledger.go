c5b7e033472922844179c4b6af0d68f91ba9bef6
TokyoDiary <106933319+TokyoDiary@users.noreply.github.com>
2022-04-15 08:23:34 +0000

    Update documentation/WindRiverHelix/WindRiverHelixLabCloud.md

--- ledger.go
+++ ledger.go
@@ -14,4 +14,4 @@ type orderEntry struct {
 }
 
 func (t orderEntry) String() string {
-	return fmt.Sprintf("%12s | %15s | %s | %4d | %9.2f", t.algoName, t.symbol, t.at.Format("2006/01/0
+	return fmt.Sprintf("%12s | %15s | %s | %4d | %9.2f", t.algoName, t.symbol, t.at.Format("2006/01/02 15:04:05"), t

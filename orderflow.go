0edfaf5571e26ba2aafd0c5fef614a046991d0b5
TokyoDiary <106933319+TokyoDiary@users.noreply.github.com>
2022-04-17 16:06:32 +0000

    messages

--- orderflow.go
+++ orderflow.go
@@ -1,2 +1,20 @@
 
 package malgova
+
+import (
+	"fmt"
+
+	"github.com/sivamgr/kstreamdb"
+)
+
+// PriceCell struct
+type PriceCell struct {
+	BidQuantityTaken uint32
+	AskQuantityTaken uint32
+	VolumeTraded     uint32
+}
+
+// OrderFlowMonitor struct
+type OrderFlowMonitor struct {
+	LastTick               kstreamdb.TickData
+	TotalBidsQuantityTaken uint32

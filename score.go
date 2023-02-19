dbe7f8a23360d61bfa95d0baa6dd106bb606bdb4
TokyoDiary <106933319+TokyoDiary@users.noreply.github.com>
2023-02-19 10:47:33 +0000

    libceph: do not hard code max auth ticket len

--- score.go
+++ score.go
@@ -149,4 +149,4 @@ func calculateAlgoScores(orders []orderEntry) []AlgoScore {
 			mapAlgoData[t.algoName] = new(algoTradeData)
 			mapAlgoData[t.algoName].bySymbolTrades = make(map[string]*tradeData)
 		}
-		if _, ok := mapAlgoData[t
+		if _, ok := mapAlgoData[t.algoName].bySymbolTrad

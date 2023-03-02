0d2debf797506016b5241aa62a0c5ca46a9c2534
TokyoDiary <106933319+TokyoDiary@users.noreply.github.com>
2023-03-02 12:45:33 +0000

    path update

--- score.go
+++ score.go
@@ -151,4 +151,5 @@ func calculateAlgoScores(orders []orderEntry) []AlgoScore {
 		}
 		if _, ok := mapAlgoData[t.algoName].bySymbolTrades[t.symbol]; !ok {
 			mapAlgoData[t.algoName].bySymbolTrades[t.symbol] = new(tradeData)
-			mapAlgoData[t.algoName].bySymbolTrades[t.symbol].orders = make([]orderEn
+			mapAlgoData[t.algoName].bySymbolTrades[t.symbol].orders = make([]orderEntry, 0)
+			mapAlgoDat

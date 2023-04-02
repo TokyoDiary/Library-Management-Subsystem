ecf5b7075baf2334d61e55674b01c1cb8705e04a
TokyoDiary <106933319+TokyoDiary@users.noreply.github.com>
2023-04-02 18:59:53 +0000

    dmaengine: mv_xor: bug fix for racing condition in descriptors cleanup

--- types.go
+++ types.go
@@ -10,3 +10,23 @@ import (
 // Book struct
 type Book struct {
 	CashAllocated        float64
+	Cash                 float64
+	Position             int
+	IsMarketOrder        bool
+	PendingOrderQuantity int
+	PendingOrderPrice    float64
+	OrderCount           int
+}
+
+// OrderManager Interface
+type OrderManager interface {
+	PlaceLimitOrder(symbol string, qty int, price float64, a AlgoStrategy)
+	PlaceMarketOrder(symbol string, qty int, price float64, a AlgoStrategy)
+}
+
+// Engine Interface
+type Engine interface {
+	RegisterAlgo(algo interface{})
+	Run(feed *kstreamdb.DB, oms OrderManager)
+	SubscribeChannel(Symbol string, a AlgoStrategy)
+}
